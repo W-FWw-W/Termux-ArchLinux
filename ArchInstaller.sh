4a49a1a21a9994846c9ea8d3657fabc71b445783
W-FWw-W <60286036+W-FWw-W@users.noreply.github.com>
2020-03-10 15:16:17 +0800

    Update ArchInstaller.sh

--- ArchInstaller.sh
+++ ArchInstaller.sh
@@ -5,5 +5,5 @@ proot --link2symlink tar -pJvxf rootfs.tar.xz -C Arch
 unset LD_PRELOAD
 rm -rf Arch/etc/resolv.conf
 echo -e "nameserver 8.8.8.8\nnameserver 240c::6666" > Arch/etc/resolv.conf
-sed -i -e '1i Server = https://mirrors.ustc.edu.cn/archlinuxarm/$arch/$repo' Arch/etc/pacman.d/mirrorlist
+sed -i -e '1i Server = http://mirrors.tuna.tsinghua.edu.cn/archlinuxarm/$arch/$repo' Arch/etc/pacman.d/mirrorlist
 curl -o start-archlinux.sh https://raw.githubusercontent.com/W-FWw-W/Termux-ArchLinux/master/start-archlinux.sh
