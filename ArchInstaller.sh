ba96a2fd67a2809309476384df2b805d33bf6120
W-FWw-W <60286036+W-FWw-W@users.noreply.github.com>
2020-03-11 18:55:26 +0800

    Update ArchInstaller.sh

--- ArchInstaller.sh
+++ ArchInstaller.sh
@@ -1,18 +1,32 @@
-#!/data/data/com.termux/files/usr/bin/bash
-termux-setup-storage
-apt install -y proot vim nano openssh wget
+#pkg up
+#升级
+#termux-setup-storage
+#读取sdcard
+#vim $PREFIX/etc/motd
+#pkg root-repo x11-repo
+#安装依赖
+apt install -y proot vim nano openssh wget git zsh
+#安装依赖
+#sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+#安装ZSH
+#cd ~
+#vim .zshrc
+# 更改Zsh主题 themes --> nanotech
 curl -o rootfs.tar.xz https://mirror.tuna.tsinghua.edu.cn/lxc-images/images/archlinux/current/arm64/default/20200310_04%3A18/rootfs.tar.xz
+#下载Arch System
 mkdir Arch
+#建立Arch文件夹
 mkdir Arch-binds
+#建立Arch-binds文件夹
 proot --link2symlink tar -pJvxf rootfs.tar.xz -C Arch
+#解压Arch System
 unset LD_PRELOAD
+#设置初始化
 rm -rf Arch/etc/resolv.conf
 echo -e "nameserver 8.8.8.8\nnameserver 240c::6666" > Arch/etc/resolv.conf
 sed -i -e '1i Server = http://mirrors.tuna.tsinghua.edu.cn/archlinuxarm/$arch/$repo' Arch/etc/pacman.d/mirrorlist
-cat >/data/data/com.termux/files/usr/bin/arch <<-EndOfFile
-#!/data/data/com.termux/files/usr/bin/bash
-cd ~
-startarch(){
+
+cat > /data/data/com.termux/files/usr/bin/Arch <<-EndOfFile
 cd $(dirname $0)
 pulseaudio -k >>/dev/null 2>&1
 pulseaudio --start >>/dev/null 2>&1
@@ -34,7 +48,7 @@ command+=" /usr/bin/env -i"
 command+=" HOME=/root"
 command+=" PATH=/usr/local/sbin:/usr/local/bin:/bin:/usr/bin:/sbin:/usr/sbin:/usr/games:/usr/local/games"
 command+=" TERM=$TERM"
-command+=" LANG=zh_CN.UTF-8"
+command+=" LANG=en_US.UTF-8"
 command+=" LC_ALL=C"
 command+=" LANGUAGE=en_US"
 command+=" /bin/bash --login"
@@ -44,6 +58,15 @@ exec $command
 else
 $command -c "$com"
 fi
-}
-chmod +x /data/data/com.termux/files/usr/bin/arch
+EndOfFile
+
+sed -i 's/#Color/Color/g' Arch/etc/pacman.conf
+
+Arch
+pacman-key --init
+pacman-key --populate
+pacman -Sy
+pacman -Syyuu
+ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
+pacman -S sudo vim neofetch tigervnc
 
