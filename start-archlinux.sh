ebbc30eb42011e19d81630d514474db9643f7655
W-FWw-W <60286036+W-FWw-W@users.noreply.github.com>
2020-03-14 14:13:12 +0800

    Update start-archlinux.sh

--- start-archlinux.sh
+++ start-archlinux.sh
@@ -5,7 +5,7 @@ unset LD_PRELOAD
 command="proot"
 command+=" --link2symlink"
 command+=" -0"
-command+=" -r Arch"
+command+=" -r Arch-rootfs"
 if [ -n "$(ls -A Arch-binds)" ]; then
 for f in Arch-binds/* ;do
 . $f
@@ -13,7 +13,7 @@ done
 fi
 command+=" -b /dev"
 command+=" -b /proc"
-command+=" -b Arch/root:/dev/shm"
+command+=" -b Arch-rootfs/root:/dev/shm"
 command+=" -w /root"
 command+=" /usr/bin/env -i"
 command+=" HOME=/root"
@@ -21,7 +21,6 @@ command+=" PATH=/usr/local/sbin:/usr/local/bin:/bin:/usr/bin:/sbin:/usr/sbin:/us
 command+=" TERM=$TERM"
 command+=" LANG=zh_CN.UTF-8"
 command+=" LC_ALL=C"
-command+=" LANGUAGE=en_US"
 command+=" /bin/bash --login"
 com="$@"
 if [ -z "$1" ];then
