7b543030f02fc97e67f152475a83369c5ba5941c
W-FWw-W <60286036+W-FWw-W@users.noreply.github.com>
2020-03-15 21:58:32 +0800

    Update ArchInstaller.sh

--- ArchInstaller.sh
+++ ArchInstaller.sh
@@ -8,7 +8,7 @@ rm -rf $PREFIX/etc/motd
 #
 pkg install x11-repo
 #安装依赖
-apt install proot procps pv wget curl aria2 vim openssh git zsh
+apt install -y proot vim nano openssh wget git zsh
 #安装依赖
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 #安装ZSH
@@ -16,17 +16,67 @@ git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/the
 #
 sed -i 's:ZSH_THEME="robbyrussell":ZSH_THEME="powerlevel10k/powerlevel10k":' .zshrc
 # 更改Zsh主题 themes --> ZSH_THEME="powerlevel10k/powerlevel10k"
-
-#创建必要文件夹，防止挂载失败
-mkdir Arch-rootfs
+cd ~
+curl -o rootfs.tar.xz https://mirror.tuna.tsinghua.edu.cn/lxc-images/images/archlinux/current/amd64/default/20200312_04%3A18/rootfs.tar.xz
+#下载Arch System
+mkdir Arch-Rootfs
+#建立Arch文件夹
 mkdir Arch-binds
-wget https://mirror.tuna.tsinghua.edu.cn/lxc-images/images/archlinux/current/arm64/default/20200313_04%3A18/rootfs.tar.xz
-proot --link2symlink tar -pJx rootfs.tar.xz -C Arch-rootfs
+#建立Arch-binds文件夹
+proot --link2symlink tar -pJvxf rootfs.tar.xz -C Arch-Rootfs
+#解压Arch System
+unset LD_PRELOAD
+#设置初始化
+
 wget https://github.com/W-FWw-W/Termux-ArchLinux/raw/master/start-archlinux.sh
+
+rm -rf Arch-Rootfs/etc/resolv.conf
+rm -rf Arch-Rootfs/home/alarm
+echo -e "nameserver 8.8.8.8\nnameserver 240c::6666" > Arch-Rootfs/etc/resolv.conf
+sed -i -e '1i Server = http://mirrors.tuna.tsinghua.edu.cn/archlinuxarm/$arch/$repo' Arch-Rootfs/etc/pacman.d/mirrorlist
+sed -i 's/#Color/Color/g' Arch-Rootfs/etc/pacman.conf
+
+vim Arch-Rootfs/etc/locale.gen
+#en_US
+vim Arch-Rootfs/etc/locale.conf
+#LANG=en_US.UTF-8
+
+vim Arch-Rootfs/etc/hostname
+#LinuxArm-FWw
+vim Arch-Rootfs/etc/hosts
+#127.0.0.1  localhost
+#127.0.1.1  LinuxArm-FWw
+#::1  localhost
+
 chmod +x start-archlinux.sh
+vim start-archlinux.sh
+./start-archlinux.sh
 
-rm -rf Arch/etc/resolv.conf
-echo -e "nameserver 8.8.8.8\nnameserver 240c::6666" > Arch-rootfs/etc/resolv.conf
-sed -i -e '1i Server = http://mirrors.tuna.tsinghua.edu.cn/archlinuxarm/$arch/$repo' Arch-rootfs/etc/pacman.d/mirrorlist
-sed -i 's/#Color/Color/g' Arch-rootfs/etc/pacman.conf
+ln -sf /proc/mounts /etc/mtab
+ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
+locale-gen
+userdel alarm
+useradd -m -g users -G wheel fww
+passwd fww
+passwd root
 
+
+pacman-key --init
+pacman-key --populate
+pacman -Sy
+pacman -Syyuu
+vim ~/.bashrc
+#neofetch
+
+
+#pacman -S base base-devel git go
+#
+#git clone https://aur.archlinux.org/yay.git
+#pacman -Rn fakeroot
+#pacman -S fakeroot-tcp
+#cd yay
+#su fww -c "makepkg"
+
+
+#pacman -S xfce4 startxfce4
+#
